34983bdaeaff284b4a130612882c29843b2cdcf4
root <root@ip-172-31-84-6.ec2.internal>
2023-05-01 06:21:20 +0000

    first commit

---
+++ src/main/java/com/chandu/www/App.java
@@ -0,0 +1,13 @@
+package com.chandu.www;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hi this is my maven projet" );
+    }
+}
